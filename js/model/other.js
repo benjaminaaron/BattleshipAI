52bb4f4be00fc5adc55f432464fe4ff6011e372f
steff <sehrenbe@hm.edu>
2015-06-17 13:34:07 +0000

    small stuff

--- js/model/other.js
+++ js/model/other.js
@@ -9,7 +9,7 @@ var CellStatus = {
     MINE: 5,
     RADIATION: 6,    // indicates mine on adjacent cell
     WAVE: 7
-}
+};
 
 /** 
 * Important for GUI to decide whether the canvas needs to be redrawn. 
@@ -23,28 +23,29 @@ var UpdateReport = {
     ONESETUPCOMPLETED: 5,
     ONETURNCOMPLETED: 6,
     GAMECOMPLETED: 7
-}
+};
 
+// TODO Umbenennen in CustomMouseEvent oder aehnliches!
 var MouseEvent = {
     MOUSEDOWN: 0,
     MOUSEMOVE: 1,
     MOUSEUP: 2
-}
+};
 
 var CellStatusMsg = function(status) {
     this.status = status;
     this.destroyedShipCode; // if there is one
     this.allShipsDestroyed = false;
-}
+};
 
 var ShipPos = function(orientation, row, col){
     this.orientation = orientation;
     this.row = row;
     this.col = col;
-}
+};
 
 var ShipType = function(size, color, quantity){
     this.size = size;
     this.color = color;
     this.quantity = quantity;
-}
+};
