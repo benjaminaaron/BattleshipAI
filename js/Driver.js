2552481d6ce23eef8ad12577c0b952130ca8922c
stehrenberg <sehrenbe@hm.edu>
2015-06-22 18:25:33 +0200

    further refactoring Driver

--- js/Driver.js
+++ js/Driver.js
@@ -13,34 +13,14 @@ var game, viewModule;
 * Handles the overall logic required for starting the game.
 */
 function startGame(){
-    var player0 = player1 = null;
 
-    switch($('#gameOptions').val()){
-        case 'hb':
-            var name = prompt('Enter your name:', 'Max');
-            player0 = new Human(name);
-            var player1 = createBot();       
-            break;          
-        case 'hh': 
-            var name = prompt('Enter the name of Player 1:', 'Max');
-            player0 = new Human(name);
-            name = prompt('Enter the name of Player 2:', 'Erika');
-            player1 = new Human(name);  
-            break;
-        case 'bb':
-            player0 = new createBot();
-            player1 = new createBot();
-            break;
-    }
+    initializePlayers();
+    handleButtonDisplay();
+    showStatusInfo('in <b>setup phase</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
 
-    $('#gameOptions').hide();
-    $('#startBtn').hide();
-    $('#gameviewRadioBtn').hide();
-    $('#statsviewRadioBtn').hide();
+    // setting px width for tool bar "gameControls"
+    var gameControlsWidth = 744;
     
-    $('#resetBtn').show();
-    $('#statusLabel').html('in <b>setup phase</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
-    var gameControlsWidth = (player1 ? 2 : 1) * 370 + (player1 ? 4 : 0);
     $('#gameControls').css('cssText', 'width: ' + gameControlsWidth + 'px !important');
 
     var shipTypes = [new ShipType(1, 'black', 1), new ShipType(5, 'aqua', 1), new ShipType(4, 'maroon', 1), new ShipType(3, 'lime', 1), new ShipType(2, 'orange', 2)];
@@ -59,11 +39,64 @@ function startGame(){
 }
 
 /**
-* Initializes the bot object/s.
+* Initializes Player objects as human players or bots, depending on input from the browser.
 */
+function initializePlayers() {
+
+    var player0 = player1 = null;
+    var name;
+
+    switch($('#gameOptions').val()){                            // DOM object containing a drop down menu where you can choose the playing mode
+        case 'hb':
+            name = prompt('Enter your name:', 'Max');
+            player0 = new Human(name);
+            player1 = createBot();       
+            break;          
+        case 'hh': 
+            name = prompt('Enter the name of Player 1:', 'Max');
+            player0 = new Human(name);
+            name = prompt('Enter the name of Player 2:', 'Erika');
+            player1 = new Human(name);  
+            break;
+        case 'bb':
+            player0 = new createBot();
+            player1 = new createBot();
+            break;
+    }
+}
+
+/**
+* Displays only the buttons neccessary for the game initialization phase.
+*/
+function handleButtonDisplay() {
+
+    $('#gameOptions').hide();
+    $('#startBtn').hide();
+    $('#gameviewRadioBtn').hide();
+    $('#statsviewRadioBtn').hide();
+    $('#resetBtn').show();
+}
+
+/**
+* Displays the appropriate text within the status info field inside the game's toolbar.
+*/
+function showStatusInfo(statusMsg) {
+    $('#statusLabel').html(statusMsg);
+}
+
 function createBot(){
-    var type = prompt('Choose the type of bot\n\nr      RandomBot\nrd    RandomDestroyerBot\nzsd  ZoningSquareDestroyerBot\nzrd  ZoningRectDestroyerBot\n', 'r');           
+
     var bot = false;
+    var type = prompt('
+        Choose the type of bot\n\nr
+        RandomBot\nrd
+        RandomDestroyerBot\nzsd
+        ZoningSquareDestroyerBot\nzrd
+        ZoningRectDestroyerBot\n
+        ', 
+        'r'                     // default text displayed inside prompt
+        );
+
     switch(type){
         case 'r':
             bot = new RandomBot('random bot');
@@ -78,11 +111,29 @@ function createBot(){
             bot = new ZoningRectDestroyerBot('zoning rect destroyer bot');
             break;
     }
+
     if(!bot){
         alert('invalid input or that type of bot isn\'t implemented yet, random bot is chosen instead');
-        bot = new RandomBot('random bot');  
-    } 
+        bot = new RandomBot('random bot');
+    }
+
     return bot; 
+}
+
+/**
+* Shows an alert box with information on how to use the battleship application.
+*/
+function info(){
+    var str = 'Setup phase:\n' +
+  '- drag ships to place them\n' + 
+  '- click on a ship will flip its orientation\n' +
+  '- click on the field will randomly place all ships\n' +
+  '- click on your board outside of the field means you finished setting up ships\n\n' +
+  'Play phase:\n' +
+  '- just go ahead shooting around on the opponents field\n\n' + 
+  'Build by:\n@stehrenberg\n@chanton1392\n@abstractCalculus\n@benjaminaaron\n\n' + 
+  'More info at:\nhttps://github.com/benjaminaaron/BattleshipAI';
+  alert(str);
 }
 
 /** 
@@ -92,39 +143,12 @@ function draw(){
     window.requestAnimationFrame(viewModule.draw());
 }
 
-function info(){
-    var str = '<b>Setup phase</b>:<br>' +
-  '- drag ships to place them<br>' + 
-  '- click on a ship will flip its orientation<br>' +
-  '- click on the field will randomly place all ships<br>' +
-  '- click on your board outside of the field means you finished setting up ships<br><br>' +
-  '<b>Play phase</b>:<br>' +
-  '- just go ahead shooting around on the opponents field<br><br>' + 
-  '<b>Build by</b>:<br>@stehrenberg<br>@chanton1392<br>@abstractCalculus<br>@benjaminaaron<br><br>' + 
-  'More info at:<br>https://github.com/benjaminaaron/BattleshipAI';
-  alert(str);
-}
 
-// UTILITY FUNCTIONS
+////////////////////////////// UTILITY FUNCTIONS //////////////////////////////
 
+/**
+* Performs a simple page reload
+*/
 function reset(){
     location.reload();
-}
-
-function ensure2Digits(content){
-    var str = '' + content;
-    if(str.length == 1)
-        str = '0' + str;
-    return str;
-}
-
-function getFormattedDate() {
-    var date = new Date();
-    var day = date.getDate();
-    var month = (date.getMonth() + 1);
-    var year = date.getFullYear();
-    var hour = date.getHours();
-    var minute = date.getMinutes();
-    var second = date.getSeconds();           
-    return ensure2Digits(day) + '.' + ensure2Digits(month) + '.' + ensure2Digits(year) + ' ' + ensure2Digits(hour) + ':' + ensure2Digits(minute) + ':' + ensure2Digits(second);
 }
